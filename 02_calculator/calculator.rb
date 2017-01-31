e58babf336b05531f8ecd61d62a79ce4dc27be69
Jason Mabry <jmabry@mymabry.com>
2017-01-31 12:01:59 -0500

    02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,46 @@
-#write your code here
+def add(a, b)
+  a+b
+end
+
+def subtract(a, b)
+  a-b
+end
+
+def sum(array)
+  sum = 0
+  array.each do |a|
+    sum += a
+  end
+  sum
+end
+
+def factorial(number)
+  if number == 0
+    factorial = number
+  else
+    count = 0
+    factorial = 1
+
+    number.times do
+      count += 1
+      factorial *= count
+    end
+  end
+  factorial
+end
+
+def multiply(array)
+  product = 1
+  array.each do |a|
+    product *= a
+  end
+  product
+end
+
+def power(num, power)
+  result = 1
+  power.times do
+    result *= num
+  end
+  result
+end
