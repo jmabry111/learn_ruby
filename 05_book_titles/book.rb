9260b5b4ea71aeaee62b132f2516333d2c3e9c0b
Jason Mabry <jmabry@mymabry.com>
2017-02-02 15:01:17 -0500

    05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
 # write your code here
+  def title
+    @title
+  end
+
+  def title=(title)
+    @title = titleize(title)
+  end
+
+  private
+  def titleize(title)
+    lowered_words = %w{and but or for the a an in of}
+    title.capitalize.split.map{|w| lowered_words.include?(w) ? w : w.capitalize}.join(" ")
+  end
 end
