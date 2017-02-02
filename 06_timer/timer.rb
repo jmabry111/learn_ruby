b501b54de20180382406bfa1f134d9669ad45835
Jason Mabry <jmabry@mymabry.com>
2017-02-02 15:28:52 -0500

    06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds 
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    string = []
+    secs = (@seconds%60)
+    mins = (@seconds/60)%60
+    hrs = (@seconds/3600)
+
+    string = "#{pad(hrs)}:#{pad(mins)}:#{pad(secs)}"
+  end
+
+  def pad(number)
+    (number).to_s.length > 1 ? number.to_s : "0" + number.to_s
+  end
 end
